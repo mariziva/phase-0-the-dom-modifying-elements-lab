917772a02131b6622de9bed8707dbc5317a88cd8
Mari Taksa <welch.mari@gmail.com>
2021-06-08 12:40:31 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector("main#main").remove(); 
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader); 
+newHeader.id= "victory";
+newHeader.innerHTML= "<h1>Mari is the champion</h1>";
